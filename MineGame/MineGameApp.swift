4d8bce7d5f6a9ae239274542b0f2afba579054a7
lemonshark <zmeng8@usfca.com>
2022-12-30 23:40:29 -0800

    Initial Commit

---
+++ MineGame/MineGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MineGameApp.swift
+//  MineGame
+//
+//  Created by lemonshark on 2022/12/30.
+//
+
+import SwiftUI
+
+@main
+struct MineGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
